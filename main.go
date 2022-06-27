03c4d2237ab3569c6c60f0c65119f9495a20d7a7
Ryan Talbot <ryan-talbot@outlook.com>
2022-06-27 19:19:02 +0100

    add setting of namespace

--- main.go
+++ main.go
@@ -4,27 +4,43 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	case "child":
+		child()
 	default:
-		panic("bad command")
+		panic("Boo")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	command := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	must(command.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
 
 	command := exec.Command(os.Args[2], os.Args[3:]...)
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 
-	command.Run()
+	must(command.Run())
 }
 
 func must(err error) {
